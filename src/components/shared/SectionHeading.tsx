214da71125736e78a20982ae8b7d62a9f89c436f
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-04-03 20:07:25 +0600

    refactor(section heading): make responsive

--- src/components/shared/SectionHeading.tsx
+++ src/components/shared/SectionHeading.tsx
@@ -5,8 +5,8 @@ type HeadingProps = {
 
 const SectionHeading = ({ subHeading, heading }: HeadingProps) => {
 	return (
-		<div className="mb-12 text-center w-sm mx-auto">
-			<p className="text-[#D99904] italic text-xl">---{subHeading}---</p>
+		<div className="mb-12 text-center w-60 md:w-sm mx-auto">
+			<p className="text-[#D99904] italic text-sm md:text-xl">---{subHeading}---</p>
 			<div className="mt-5">
 				<h2 className="uppercase py-5 text-[#151515] text-xl md:text-2xl lg:text-4xl 2xl:text-[40px] border-y-2 border-[#E8E8E8]">
 					{heading}
